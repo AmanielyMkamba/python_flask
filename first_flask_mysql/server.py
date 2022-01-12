c8ad0bc4d0ac3ad5a4660d02328fa8a46fe1df69
Amani Mkamba <88519140+AmanielyMkamba@users.noreply.github.com>
2022-01-11 22:27:23 -0500

    Add files via upload

---
+++ first_flask_mysql/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+# import the class from friend.py
+from friend import Friend
+app = Flask(__name__)
+
+"""
+@app.route("/")
+def index():
+    # call the get all classmethod to get all friends
+    friends = Friend.get_all()
+    print(friends)
+    return render_template("index.html")
+"""
+
+#Add this new route to retrieve data from database and display on browser
+@app.route('/')
+def index():
+    friends = Friend.get_all()
+    print(friends)
+    return render_template("index.html", all_friends = friends)
+
+if __name__ == "__main__":
+    app.run(debug=True)
