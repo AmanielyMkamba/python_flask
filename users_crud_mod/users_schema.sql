7602cf68dff434ac379ea1966a05e66d8e67352f
Amani Mkamba <88519140+AmanielyMkamba@users.noreply.github.com>
2022-01-24 22:51:50 -0500

    Add files via upload

---
+++ users_crud_mod/users_schema.sql
@@ -0,0 +1,16 @@
+#SELECT * FROM users_schema.users;
+
+#INSERT INTO users_schema.users (first_name, last_name, email)
+#VALUES('Bob', 'Marley', 'bob@marley.com');
+
+#SELECT * FROM users_schema.users;
+
+#INSERT INTO users_schema.users (first_name, last_name, email)
+#VALUES ('Hulk', 'Hogon', 'hulk@hogon.com')
+
+#SELECT * FROM users_schema.users;
+
+#INSERT INTO users_schema.users (first_name, last_name, email)
+#VALUES ('Amani', 'Mkamba', 'amani@mkamba.com')
+
+SELECT * FROM users_schema.users;
